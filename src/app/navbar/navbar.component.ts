0fe09364f0a39bff4183383482b705914bb564d1
Nativ Lengel <nativlengel11@gmail.com>
2021-08-01 20:07:23 +0300

    UPDATE navbar - animation

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,3 @@
-import { style } from '@angular/animations';
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,18 +5,34 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit{
   
   drop_down : boolean = false;
-  
+  menu_icon : HTMLElement;
   constructor() { }
   
   dropMenuDown() : void {
     this.drop_down == true ? this.drop_down = false : this.drop_down = true;
+
+    this.menu_icon.style.animationName = 'navbar-menu-icon-animation';
+    setTimeout(() => {
+      this.menu_icon.style.animationName = '';
+    }, 1000);
+
   }
 
   ngOnInit(): void {
     
+  }
+  ngAfterViewInit(): void {
+    this.menu_icon = document.getElementById('navbar-mobile-menu-icon'); 
+    const lis = document.getElementsByClassName('separator') as HTMLCollectionOf<HTMLElement>;
+    for(let i=0; i<lis.length; i++)
+    {
+      i % 2 == 0 ? lis[i].style.animationName = 'navbar-menu-entry-separator-animation-left' : lis[i].style.animationName = 'navbar-menu-entry-separator-animation-right';
+      lis[i].style.animationDuration = `${1.2}s`;
+      lis[i].style.animationDelay='0.2s';
+    }
   }
 
 }
