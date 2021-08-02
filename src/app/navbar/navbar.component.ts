f0de25a097e9ae43f1233241a633155de43ebc34
Nativ Lengel <Nativ.Lengel@wdc.com>
2021-08-02 10:59:48 +0300

    UPDATE navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,30 +9,55 @@ export class NavbarComponent implements OnInit, AfterViewInit{
   
   drop_down : boolean = false;
   menu_icon : HTMLElement;
+  lis : HTMLCollectionOf<HTMLElement>;
   constructor() { }
   
   dropMenuDown() : void {
-    this.drop_down == true ? this.drop_down = false : this.drop_down = true;
-
     this.menu_icon.style.animationName = 'navbar-menu-icon-animation';
+    const navbar_menu_entries_container = document.getElementById('navbar_menu_entries_container');
     setTimeout(() => {
       this.menu_icon.style.animationName = '';
     }, 1000);
+    if(this.drop_down == false)
+    {
+      for(let i=0; i<this.lis.length; i++)
+    {
+      i % 2 == 0 ? this.lis[i].style.animationName = 'navbar-menu-entry-separator-animation-left' : this.lis[i].style.animationName = 'navbar-menu-entry-separator-animation-right';
+      this.lis[i].style.animationDuration = `${1.2}s`;
+      this.lis[i].style.animationDelay='0.2s';
+    }
+      navbar_menu_entries_container.style.animationName = 'navbar-menu-entries-container-animation';
+      setTimeout(() => {
+        navbar_menu_entries_container.style.animationName = '';
+        
+      }, 1000);  
+      this.drop_down = true;  
+    }
+    else {
+      
+      for(let i=0; i<this.lis.length; i++)
+      {
+        i % 2 == 0 ? this.lis[i].style.animationName = 'navbar-menu-entry-separator-animation-left-close' : this.lis[i].style.animationName = 'navbar-menu-entry-separator-animation-right-close';
+        this.lis[i].style.animationDuration = `${1}s`;
+        this.lis[i].style.animationDelay='0s';
+      }
+      navbar_menu_entries_container.style.animationName = 'navbar-menu-entries-container-animation-close';
+      setTimeout(() => {
+        navbar_menu_entries_container.style.animationName = '';
+        
+      this.drop_down = false;
+      }, 1000);
+    }
 
   }
 
   ngOnInit(): void {
     
   }
+
   ngAfterViewInit(): void {
-    this.menu_icon = document.getElementById('navbar-mobile-menu-icon'); 
-    const lis = document.getElementsByClassName('separator') as HTMLCollectionOf<HTMLElement>;
-    for(let i=0; i<lis.length; i++)
-    {
-      i % 2 == 0 ? lis[i].style.animationName = 'navbar-menu-entry-separator-animation-left' : lis[i].style.animationName = 'navbar-menu-entry-separator-animation-right';
-      lis[i].style.animationDuration = `${1.2}s`;
-      lis[i].style.animationDelay='0.2s';
-    }
+    this.lis = document.getElementsByClassName('separator') as HTMLCollectionOf<HTMLElement>;
+    this.menu_icon = document.getElementById('navbar-menu-icon');
   }
 
 }
